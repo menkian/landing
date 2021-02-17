e351925bd1a65081deb73f82d699b798b5af08af
menkian <menkian@outlook.com>
2021-02-16 20:47:23 -0500

    Final 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,10 +24,10 @@
 
         if (window.pageYOffset > sticky) {
             header_navbar.classList.add("sticky");
-            logo.src = 'assets/images/logo/logo-2.svg';
+            logo.src = 'assets/images/logo/logo-2-01.png';
         } else {
             header_navbar.classList.remove("sticky");
-            logo.src = 'assets/images/logo/logo.svg';
+            logo.src = 'assets/images/logo/logo-01.png';
         }
 
 
